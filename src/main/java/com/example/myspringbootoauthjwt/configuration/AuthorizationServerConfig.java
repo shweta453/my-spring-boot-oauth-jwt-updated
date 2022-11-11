e12d3a9db98c0ed74ee26f2d3c396e0387217f0b
shweta@453 <sdm23231470@gmail.com>
2022-11-11 22:18:49 +0530

    Added code

--- src/main/java/com/example/myspringbootoauthjwt/configuration/AuthorizationServerConfig.java
+++ src/main/java/com/example/myspringbootoauthjwt/configuration/AuthorizationServerConfig.java
@@ -1,40 +1,84 @@
 package com.example.myspringbootoauthjwt.configuration;
 
-import com.example.myspringbootoauthjwt.service.Impl.UserDetailsImpl;
-import com.example.myspringbootoauthjwt.service.Impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.OAuth2RequestFactory;
-import org.springframework.security.oauth2.provider.endpoint.TokenEndpointAuthenticationFilter;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+    private String clientid = "tutorialspoint";
+    private String clientSecret = "my-secret-key";
+    private String privateKey = "private key";
+    private String publicKey = "public key";
 
     private static final String RESOURCE_ID = "resource_id";
+
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    public JwtAccessTokenConverter tokenEnhancer() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("as466gf");
+        return converter;
+    }
+    @Bean
+    public JwtTokenStore tokenStore() {
+        return new JwtTokenStore(tokenEnhancer());
+    }
+
+    @Primary
+    @Bean
+    public AuthorizationServerTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenEnhancer(tokenEnhancer());
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        defaultTokenServices.setAccessTokenValiditySeconds(60);
+        defaultTokenServices.setRefreshTokenValiditySeconds(60);
+        return defaultTokenServices;
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore())
+                .accessTokenConverter(tokenEnhancer());
+        endpoints.tokenServices(tokenServices());
+
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+    }
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient(clientid).secret(bCryptPasswordEncoder.encode(clientSecret)).scopes("read", "write")
+                .authorizedGrantTypes("password", "refresh_token").accessTokenValiditySeconds(1)
+                .resourceIds(RESOURCE_ID);
+
+    }
+
+
+    /*private static final String RESOURCE_ID = "resource_id";
     @Autowired
     @Qualifier("authenticationManagerBean")
     AuthenticationManager authenticationManager;
@@ -49,6 +93,18 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     TokenStore tokenStore = new InMemoryTokenStore();
 
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("as466gf");
+        return converter;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
@@ -69,6 +125,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         DefaultTokenServices tokenServices = new DefaultTokenServices();
         tokenServices.setTokenStore(this.tokenStore);
         return tokenServices;
-    }
+    }*/
 
 }
--- src/main/java/com/example/myspringbootoauthjwt/configuration/ResourceServerConfig.java
+++ src/main/java/com/example/myspringbootoauthjwt/configuration/ResourceServerConfig.java
@@ -1,11 +1,17 @@
 package com.example.myspringbootoauthjwt.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableResourceServer
@@ -13,9 +19,28 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     private static final String RESOURCE_ID = "resource_id";
 
+
+    /*@Autowired
+    public JwtTokenStore tokenStore;
+
+    @Autowired
+    public JwtAccessTokenConverter tokenEnhancer;
+
+
+    public ResourceServerTokenServices tokenServices(){
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenEnhancer(tokenEnhancer);
+        defaultTokenServices.setTokenStore(tokenStore);
+        defaultTokenServices.setSupportRefreshToken(true);
+        defaultTokenServices.setAccessTokenValiditySeconds(3600);
+        defaultTokenServices.setRefreshTokenValiditySeconds(3600);
+        return defaultTokenServices;
+    }
+*/
     @Override
-    public void configure(ResourceServerSecurityConfigurer resources) {
-        resources.resourceId(RESOURCE_ID).stateless(false);
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        //resources.tokenServices(tokenServices());
+        resources.resourceId(RESOURCE_ID);
     }
 
     @Override
